aabc5c79e64bfb49501a408b4a69f9d107ea682f
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2019-10-12 18:30:32 +0200

    transferring

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copy config files. Create a backup files or `--no-backup' to overwrite
+# existing configs permanently.
+
+for file in `ls -1 | grep -v $(basename $0)` 
+do 
+	cp -v -b $file ~/.$file
+done
