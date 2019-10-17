ff50812d0a7fb0b37682fc7d03b498ff8377bb79
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2019-10-17 14:15:30 +0200

    push

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Copy config files. Create a backup files or `--no-backup' to overwrite
 # existing configs permanently.
 
+# Accept directory prefix
 for file in `ls -1 | grep -v $(basename $0)` 
 do 
 	cp -v -b $file ~/.$file
